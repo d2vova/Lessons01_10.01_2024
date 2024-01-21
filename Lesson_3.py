b1a1035f8a5815044fff818e58a0c1e4632caae6
d2vova <Vprikhodko85@gmail.com>
2024-01-21 21:11:37 +0200

    First Commit

---
+++ Lesson_3.py
@@ -0,0 +1,18 @@
+number_1 = int(input("Введіть перше число: "))
+number_2 = int(input("Введіть друге число: "))
+operaciya = input("Введіть операцію (\"+\", \"-\", \"*\", \"/\"): ")
+if operaciya == "+":
+    print("Ваш результат:")
+    print(number_1+number_2)
+elif operaciya == "-":
+    print("Ваш результат:")
+    print(number_1 - number_2)
+elif operaciya == "*":
+    print("Ваш результат:")
+    print(number_1 * number_2)
+elif operaciya == "/":
+    if number_2 != 0:
+        print("Ваш результат:")
+        print(number_1 / number_2)
+    else:
+        print("Ділити на 0 не можна. Введіть іншу цифру, або зменіть операцію")
---
+++ test_2.py
@@ -0,0 +1,9 @@
+www = "www.conquer_and_command.net"
+
+if ".com" in www:
+
+    print("com in www")
+
+else:
+
+    print("com not in www")
