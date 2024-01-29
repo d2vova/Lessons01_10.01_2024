1e792ca6a808e7f9680040772c18d9dfece112f7
d2vova <Vprikhodko85@gmail.com>
2024-01-29 13:27:40 +0200

    First Commit

--- Lesson_4_2.py
+++ Lesson_4_2.py
@@ -1,4 +1,8 @@
-list_1 = [0, 4, 5, 2, 6, 7, 4, 2, 1, 7]
-sum_list = sum(list_1[::2])
-result = sum_list * list_1[-1]
-print(result)
+list_1 = []
+if not list_1:
+    list_1 = [0]
+    print(list_1)
+else:
+    sum_list = sum(list_1[::2])
+    result = sum_list * list_1[-1]
+    print(result)
