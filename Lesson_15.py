98a2806e49a013b0e089f262ac05f257e117e771
d2vova <Vprikhodko85@gmail.com>
2024-03-01 14:55:04 +0200

    last commit

---
+++ Lesson_15.py
@@ -0,0 +1,36 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_square(self):
+        return self.width * self.height
+
+    def __eq__(self, other):
+        return self.get_square() == other.get_square()
+
+    def __add__(self, other):
+        total_square = self.get_square() + other.get_square()
+        return Rectangle(self.width, total_square / self.width)
+
+    def __mul__(self, n):
+        new_width = self.width * (n ** 0.5)
+        new_height = self.height * (n ** 0.5)
+        return Rectangle(new_width, new_height)
+
+    def __str__(self):
+        return f"Rectangle({self.width} x {self.height})"
+
+
+r1 = Rectangle(2, 4)
+r2 = Rectangle(3, 6)
+assert r1.get_square() == 8, 'Test1'
+assert r2.get_square() == 18, 'Test2'
+
+r3 = r1 + r2
+assert r3.get_square() == 26, 'Test3'
+
+r4 = r1 * 4
+assert r4.get_square() == 32, 'Test4'
+
+
